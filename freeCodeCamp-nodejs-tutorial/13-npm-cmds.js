65faabc7494dd16b29d49bc9a2dd35432d3ee5bf
Rana Salem <ranatasalem@gmail.com>
2023-11-21 18:06:32 +0000

    Adding README

--- freeCodeCamp-nodejs-tutorial/13-npm-cmds.js
+++ freeCodeCamp-nodejs-tutorial/13-npm-cmds.js
@@ -17,10 +17,22 @@
 
 // PACKAGE.JSON
 // have a look in the package.json file, you will see the 2 dependencies (lodash and bootstrap) that were both installed
-// via the command: npm i <packageName>
+// via the command: npm i <packageName
+
+// npm i <packageName> -D OR --save-dev (both of these will install a package as a dev dependency i.e. one that is not needed for production)
+// npm i nodemon -D OR npm i nodemon --save-dev (nodemon is not needed for production therefore it was installed as a dev dependency)
+// testing packages, linting, formatting etc. could all be added as dev dependencies rather than jamming them under dependencies
 
 const _ = require('lodash');
 
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+
+// VERSION CONTROL - GIT
+/* typically, the node_modules folder will be included in the .gitignore file which means they will not be pushed to the repo.
+this means that whoever clones the repo will not also clone the node_modules folder that contains all the needed dependencies,
+but this is not a problem, because after cloning the repo, simply open the terminal (make sure to be within the project itself)
+and run the command npm install. npm will check what dependencies are listed in the package.json and install them automatically 
+after running the command. This will also add in the package-lock.json file as well!
+*/
