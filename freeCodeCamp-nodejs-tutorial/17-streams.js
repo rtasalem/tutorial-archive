261851504710e94d58e6c442eb5cef380701af18
Rana Salem <ranatasalem@gmail.com>
2023-11-22 15:42:30 +0000

    Adding remaining streams examples.

--- freeCodeCamp-nodejs-tutorial/17-streams.js
+++ freeCodeCamp-nodejs-tutorial/17-streams.js
@@ -19,6 +19,7 @@ const { createReadStream } = require('fs');
 
 // creating a new variable called stream, invoking the createReadStream method and passing in the path to the file
 const stream = createReadStream('../content/big.txt', { highWaterMark: 900000, encoding: 'utf8' });
+// NOTE WHEN RUNNING THIS PROGRAM - change the above path to './content/big.txt' i.e. remove the extra dot at the start which was put there intentionally to test the program's functionality
 
 stream.on('data', (result) => {
     console.log(result);
@@ -27,3 +28,22 @@ stream.on('data', (result) => {
 stream.on('error', (err) => {
     console.log(err);
 });
+
+// -----------------------------
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8');
+    // res.end(text);
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStream.on('open', () => {
+        fileStream.pipe(res); // pushes the read stream into the write stream because if we have the ability to read data in chunks then we also have the ability to write data in chunks
+    })
+    fileStream.on('error', (err) => {
+        res.end(err);
+    });
+}).listen(4000);
+
+// the above is making the big.txt file readable on port 4000 i.e. enter localhost:4000 into browser and you should see the big.txt file printed out
