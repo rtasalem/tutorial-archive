66fac8273f119c57cc75500403f5ccd632170e0d
Rana Salem <ranatasalem@gmail.com>
2023-11-23 12:24:46 +0000

    Fixed an issue with the first instance of the HTTP module that was discussed in the tutorial. If statements are set up slightly differently compared to what was shown in the tutorial as a result.

--- freeCodeCamp-nodejs-tutorial/12-http.js
+++ freeCodeCamp-nodejs-tutorial/12-http.js
@@ -4,19 +4,16 @@ const server = http.createServer((req, res) => { // req = incoming request from 
     // console.log(req); 
     if (req.url === '/') { // triple = will not perform type conversion like double = will i.e. triple = will check the variables have the same type and value
         res.end('Welcome to our homepage');
-    };
-
-    if (req.url === '/about') {
+    } else if (req.url === '/about') {
         res.end('Here is our short history');
+    } else {
+        res.end(`
+        <h1>Oops</h1>
+        <p>We can't seem to find the page you're looking for :(</p>
+        <a href="/">back home</a>
+        `);
     }
-
-    res.end(`
-    <h1>Oops</h1>
-    <p>We can't seem to find the page you're looking for :(</p>
-    <a href="/">back home</a>
-    `);
-
-    // the above works in the YouTube tutorial, but for some reason doesn't properly execute when tried locally (disables the about page too for some reason)
+    // the above code is not the same as what was done in the YouTube tutorial, I think this is because the if statements were set up using an older version of node that does not work with the version i am using.
 });
 
 server.listen(4000); // originally the port 5000 was used but after looking online it seems that Apple is using this port for 'AirPlay'
